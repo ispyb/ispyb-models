ee0fec08e919f4a34a622f6466fb85d1449f0512
Stuart Fisher <stuart.fisher@esrf.fr>
2022-07-19 17:57:40 +0200

    back populate `UserGroup.Permission`

--- src/ispyb/models/__init__.py
+++ src/ispyb/models/__init__.py
@@ -1,3 +1,11 @@
+from sqlalchemy.orm import relationship
+
 from ._auto_db_schema import *  # noqa F403
+from ._auto_db_schema import UserGroup
 
 __version__ = "0.0.3"
+
+
+UserGroup.Permission = relationship(
+    "Permission", secondary="UserGroup_has_Permission", back_populates="UserGroup"
+)
