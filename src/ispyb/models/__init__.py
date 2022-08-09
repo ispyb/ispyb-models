ae96f3d8faa2563a0e0840a5a5a1cd1633838690
Clemente Borges <clemente.borges@desy.de>
2022-08-09 13:52:46 +0200

    Added new ProposalHasPerson and SessionHasPerson relationships (#15)

--- src/ispyb/models/__init__.py
+++ src/ispyb/models/__init__.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import relationship
 
 from ._auto_db_schema import *  # noqa F403
-from ._auto_db_schema import UserGroup
+from ._auto_db_schema import UserGroup, Proposal, BLSession
 
 __version__ = "1.0.0"
 
@@ -11,4 +11,10 @@ UserGroup.Permission = relationship(
 )
 UserGroup.Person = relationship(
     "Person", secondary="UserGroup_has_Person", back_populates="UserGroup"
+)
+Proposal.ProposalHasPerson = relationship(
+    "ProposalHasPerson", back_populates="Proposal"
+)
+BLSession.SessionHasPerson = relationship(
+    "SessionHasPerson", back_populates="BLSession"
 )
