f4e95354e75d807069317aa5866d28fbe62a59be
Stu Fisher <stuart.fisher@esrf.fr>
2022-09-14 18:46:39 +0200

    Add mappings for basic resources (#17)

--- src/ispyb/models/__init__.py
+++ src/ispyb/models/__init__.py
@@ -1,4 +1,6 @@
+from sqlalchemy import func
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.hybrid import hybrid_property
 
 from ._auto_db_schema import *  # noqa F403
 from ._auto_db_schema import UserGroup, Proposal, BLSession
@@ -12,9 +14,42 @@ UserGroup.Permission = relationship(
 UserGroup.Person = relationship(
     "Person", secondary="UserGroup_has_Person", back_populates="UserGroup"
 )
-Proposal.ProposalHasPerson = relationship(
-    "ProposalHasPerson", back_populates="Proposal"
-)
-BLSession.SessionHasPerson = relationship(
-    "SessionHasPerson", back_populates="BLSession"
-)
+
+
+class ModifiedProposal(Proposal):
+    BLSession = relationship("BLSession", back_populates="Proposal")
+    ProposalHasPerson = relationship("ProposalHasPerson", back_populates="Proposal")
+
+    @hybrid_property
+    def proposal(self):
+        return self.proposalCode + self.proposalNumber
+
+
+Proposal = ModifiedProposal
+
+
+class ModifiedBLSession(BLSession):
+    SessionHasPerson = relationship("SessionHasPerson", back_populates="BLSession")
+
+    SessionType = relationship("SessionType", back_populates="BLSession")
+
+    @hybrid_property
+    def session(self):
+        if self.Proposal:
+            return (
+                self.Proposal.proposalCode
+                + self.Proposal.proposalNumber
+                + "-"
+                + str(self.visit_number)
+            )
+        else:
+            return None
+
+    @session.expression
+    def session(cls):
+        return func.concat(
+            Proposal.proposalCode, Proposal.proposalNumber, "-", cls.visit_number
+        )
+
+
+BLSession = ModifiedBLSession
