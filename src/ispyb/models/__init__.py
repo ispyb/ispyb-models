3a1ed9209237a9a2dba2978c5bc0543af5584d1c
Stuart Fisher <stuart.fisher@esrf.fr>
2022-09-14 12:33:35 +0200

    add mapping for basic resources

--- src/ispyb/models/__init__.py
+++ src/ispyb/models/__init__.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.hybrid import hybrid_property
 
 from ._auto_db_schema import *  # noqa F403
 from ._auto_db_schema import UserGroup, Proposal, BLSession
@@ -12,9 +13,37 @@ UserGroup.Permission = relationship(
 UserGroup.Person = relationship(
     "Person", secondary="UserGroup_has_Person", back_populates="UserGroup"
 )
+Proposal.BLSession = relationship(
+    "BLSession", back_populates="Proposal"
+)
 Proposal.ProposalHasPerson = relationship(
     "ProposalHasPerson", back_populates="Proposal"
 )
 BLSession.SessionHasPerson = relationship(
     "SessionHasPerson", back_populates="BLSession"
 )
+BLSession.SessionType = relationship(
+    "SessionType", back_populates="BLSession"
+)
+
+
+def proposal(self):
+    return self.proposalCode + self.proposalNumber
+
+
+Proposal.proposal = hybrid_property(proposal)
+
+
+def session(self):
+    if self.Proposal:
+        return (
+            self.Proposal.proposalCode
+            + self.Proposal.proposalNumber
+            + "-"
+            + str(self.visit_number)
+        )
+    else:
+        return None
+
+
+BLSession.session = hybrid_property(session)
