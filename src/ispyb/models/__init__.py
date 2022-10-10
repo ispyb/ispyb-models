f32a56adf3889a71d52d877c071e3105f7b3680d
Stu Fisher <stuart.fisher@esrf.fr>
2022-10-10 08:47:46 +0200

    extra relationships (#20)

--- src/ispyb/models/__init__.py
+++ src/ispyb/models/__init__.py
@@ -3,10 +3,68 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.ext.hybrid import hybrid_property
 
 from ._auto_db_schema import *  # noqa F403
-from ._auto_db_schema import UserGroup, Proposal, BLSubSample, BLSession, Protein
+from ._auto_db_schema import (
+    AutoProcProgram,
+    AutoProcIntegration,
+    AutoProcScaling,
+    DataCollection,
+    GridInfo,
+    # ProcessingJob,
+    Screening,
+    ScreeningStrategy,
+    ScreeningOutput,
+    ScreeningStrategyWedge,
+    UserGroup,
+    BLSubSample,
+    BLSession,
+    Protein,
+    Proposal,
+)
 
 __version__ = "1.0.5"
 
+DataCollection.GridInfo = relationship(
+    "GridInfo", secondary="DataCollectionGroup", back_populates="DataCollection"
+)
+
+GridInfo.DataCollection = relationship(
+    "DataCollection", secondary="DataCollectionGroup", back_populates="GridInfo"
+)
+AutoProcProgram.AutoProcIntegration = relationship(
+    "AutoProcIntegration", back_populates="AutoProcProgram"
+)
+AutoProcIntegration.AutoProcScalingHasInt = relationship(
+    "AutoProcScalingHasInt", back_populates="AutoProcIntegration"
+)
+AutoProcScaling.AutoProcScalingStatistics = relationship(
+    "AutoProcScalingStatistics", back_populates="AutoProcScaling"
+)
+AutoProcProgram.AutoProcProgramAttachments = relationship(
+    "AutoProcProgramAttachment", back_populates="AutoProcProgram"
+)
+AutoProcScaling.AutoProcScalingStatistics = relationship(
+    "AutoProcScalingStatistics", back_populates="AutoProcScaling"
+)
+# ProcessingJob.ProcessingJobParameters = relationship(
+#     "ProcessingJobParameter", back_populates="ProcessingJob"
+# )
+# ProcessingJob.ProcessingJobImageSweeps = relationship(
+#     "ProcessingJobImageSweep", back_populates="ProcessingJob"
+# )
+
+Screening.ScreeningOutput = relationship("ScreeningOutput", back_populates="Screening")
+ScreeningOutput.ScreeningStrategy = relationship(
+    "ScreeningStrategy", back_populates="ScreeningOutput"
+)
+ScreeningOutput.ScreeningOutputLattice = relationship(
+    "ScreeningOutputLattice", back_populates="ScreeningOutput"
+)
+ScreeningStrategy.ScreeningStrategyWedge = relationship(
+    "ScreeningStrategyWedge", back_populates="ScreeningStrategy"
+)
+ScreeningStrategyWedge.ScreeningStrategySubWedge = relationship(
+    "ScreeningStrategySubWedge", back_populates="ScreeningStrategyWedge"
+)
 
 UserGroup.Permission = relationship(
     "Permission", secondary="UserGroup_has_Permission", back_populates="UserGroup"
