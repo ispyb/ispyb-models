8e4ce626dfcb70b47939a4ff835da8946c13b370
Maël Gaonach <mael.gaonach@esrf.fr>
2023-01-19 14:15:24 +0100

    update script

---
+++ generate_models.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+sqlacodegen mysql+mysqlconnector://test:test@127.0.0.1/ispyb_build --nojoined --noinflect --outfile src/ispyb/models/_auto_db_schema.py
+black src/ispyb/models/_auto_db_schema.py
+patch -p1 src/ispyb/models/_auto_db_schema.py < ../../../patches/models.patch
+rm src/ispyb/models/_auto_db_schema.py.orig
